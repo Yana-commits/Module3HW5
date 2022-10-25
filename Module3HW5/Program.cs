e0e3532543d1769c67b1ca371bd35ac2a2ae5671
Yana-commits <kovriginayana60@gmail.com>
2022-10-25 10:49:21 +0300

    Initial commit

---
+++ Module3HW5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+
+internal class Program
+{
+    public static string firstPart => "TextFileHello.txt";
+    public static string secondPart => "TextFileWorld.txt";
+
+
+    async static Task Main(string[] args)
+    {
+        var task1 = ReadTextPart(firstPart);
+        var task2 = ReadTextPart(secondPart);
+         
+       var texts= await Task.WhenAll(task1, task2);
+
+        Console.WriteLine($"{string.Join(string.Empty, texts)}");
+
+    }
+
+
+    public static async Task<string> ReadTextPart(string path)
+    {
+        using (StreamReader reader = new StreamReader(path))
+        {
+            string text = await reader.ReadToEndAsync();
+            return text;
+        }
+    }
+}
